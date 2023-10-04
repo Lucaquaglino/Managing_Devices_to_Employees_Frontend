90fafeb230d4faed178000963c4f8b823e17167a
Luca Quaglino <quagli.luca@gmail.com>
2023-10-04 03:42:00 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Managing_Devices_to_Employees';
+}
